e8d05c192dfae6e8f81a9db84a0814dbf2fdeccb
Ryan Schostag <ryan.schostag@gmail.com>
2025-01-25 16:30:33 -0800

    Feat: Added more automated tests to showcase its durability with different input types

--- src/tests/test_pipeline.py
+++ src/tests/test_pipeline.py
@@ -5,24 +5,43 @@ import pytest
 from src import pipeline
 
 
+class SampleModel:
+    text:str = ''
+
+    def __init__(self, text:str=text):
+        if not isinstance(text, str):
+            raise pipeline.ValidationError(
+                f'text: {text}\n'
+                f'type: {type(text)}\n'
+                f'expected: {str}'
+            )
+        self.text:str = text
+
+
 sample_func_1 = lambda x: x + 1
 sample_func_2 = lambda x: x ** 2
 sample_func_3 = lambda x: x ** 3
-sample_func_4 = lambda x: str(x) + str(x)
+sample_func_4 = lambda x: (str(x) + str(x)).strip()
+sample_func_5 = lambda x: SampleModel(x)
+sample_func_6 = lambda x: f'<header>{x.text}</header>'
 input_data_1 = 1
+input_data_2 = 'I am '
 steps_1 = [sample_func_1]
 steps_2 = [sample_func_1, sample_func_2]
 steps_3 = [sample_func_1, sample_func_2, sample_func_3]
 steps_4 = [sample_func_1, sample_func_4]
+steps_5 = [sample_func_4, sample_func_5, sample_func_6]
 expected_output_1 = 2
 expected_output_2 = 4
 expected_output_3 = 64
 expected_output_4 = '22'
+expected_output_5 = '<header>I am I am</header>'
 @pytest.mark.parametrize('steps, input_data, expected_output', [
     (steps_1, input_data_1, expected_output_1),
     (steps_2, input_data_1, expected_output_2),
     (steps_3, input_data_1, expected_output_3),
-    (steps_4, input_data_1, expected_output_4)
+    (steps_4, input_data_1, expected_output_4),
+    (steps_5, input_data_2, expected_output_5)
 ])
 def test_execute(steps, input_data, expected_output):
     '''
